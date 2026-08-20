b049f6529dfbaa355754a848edc276229ac41d1a
Parth-2701 <parth.230101085@iiitbh.ac.in>
2026-08-21 01:43:38 +0530

    Multi chats on same page enabled modified

--- chatbot_with_multiple_chats_option/frontend.py
+++ chatbot_with_multiple_chats_option/frontend.py
@@ -11,9 +11,9 @@ def generate_thread_id():
 
 def reset_chat():
     thread_id = generate_thread_id()
-    st.session_state['message_history'] = []
-    add_thread(st.session_state['thread_id'])
     st.session_state['thread_id'] = thread_id
+    st.session_state['message_history'] = []
+    add_thread(thread_id)
 
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
